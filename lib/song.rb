dcc3a2b5dcfa661847c89a2a9306fef603a99563
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-12 03:34:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
   def self.new_from_filename(mp3_format)
     formatted_name = mp3_format.split(" - ")[1].split(".")[0]
     self.new_by_name(formatted_name)
-    artist_name = 
+    artist_name = mp3_format.split(" - ")[0]
     
   end
   
