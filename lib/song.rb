bbd922ed49469461467a1e875e8cd3248ebf3d67
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-12 03:14:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,40 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(str_name)
+    song = self.new
+    song.name = str_name
+    song
+  end
+  
+  def self.create_by_name(str_name)
+    song = self.new
+    song.name = str_name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(str_name)
+    @@all.find{|song| song.name == str_name}
+  end
+  
+  def self.find_or_create_by_name(str_name)
+    if Song.find_by_name(str_name) == nil
+      Song.create_by_name(str_name)
+    else
+      Song.find_by_name(str_name)
+    end
+  end
+  
+  def self.alphabetical
+    puts @@all.sort_by{|song| song.name}
+  end
 
 end
